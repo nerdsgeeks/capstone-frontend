306956fa064ec4f38148154252b23e7fbe9fee25
Cylvia <cylvia.lian@gmail.com>
2024-02-24 22:51:02 -0800

    room detail header done

--- src/index.jsx
+++ src/index.jsx
@@ -1,15 +1,22 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import NavigationTab from "./Navigation/NavigationTab";
+import RoomDetailHeader from "./components/RoomDetailHeader/RoomDetailHeader";
 
 const App = () => {
   handleClick = () => {
     console.log("Button clicked");
   };
 
+  const room = {
+    type: 'Suite',
+    number: 'A204',
+    status: 'checkedIn',
+  }
+
   return (
     <NavigationContainer>
-      <NavigationTab />
+      <RoomDetailHeader room={room} taskStatus="Pending"/>
     </NavigationContainer>
   );
 };
