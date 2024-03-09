68d021ed7be98d718115a8dfbf27f6399f4a6fba
Pujan Shakya <puzann9843318452@gmail.com>
2024-03-09 04:27:20 -0800

    SCRUM-194 feat: Housekeeper Profile and Request Screen

--- src/screens/Housekeeper/HousekeeperProfile.jsx
+++ src/screens/Housekeeper/HousekeeperProfile.jsx
@@ -1,21 +1,153 @@
 import React, { useState } from "react";
-import { Button, StyleSheet, Text, View } from "react-native";
+import { Button, StyleSheet, Text, View, Image } from "react-native";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+import { LinearGradient } from "expo-linear-gradient";
+import ClockShiftIcon from "../../SVG/ClockShiftIcon";
+import CalendarIcon from "../../SVG/CalendarIcon";
+import Typography from "../../components/Typography/Typography";
+import NavTabs from "../../components/NavTabs/NavTabs";
+import ScheduleList from "../../components/ScheduleList/ScheduleList";
+import BigButton from "../../components/BigButton/BigButton";
+import BedIcon from "../../SVG/BedIcon";
+import ClockIcon from "../../SVG/ClockIcon";
+import { colors } from "../../../themes/themes";
+import StarIcon from "../../SVG/StarIcon";
 
 const HousekeeperProfile = ({ navigation }) => {
+  const [activeTab, setActiveTab] = useState(0);
+  const [showScheduleTab, setShowScheduleTab] = useState(true);
+  const tabs = [{ label: "Schedule" }, { label: "Perfomance" }];
+  const scheduleList = [
+    { day: "Tue", date: "Feb 20", time: "10am-6pm" },
+    { day: "Wed", date: "Feb 21", time: "10am-6pm" },
+    { day: "Fri", date: "Feb 23", time: "8am-4pm" },
+    { day: "Sun", date: "Feb 25", time: "1pm-6pm" },
+    { day: "Mon", date: "Feb 26", time: "8am-4pm" },
+  ];
+
+  const handleTabPress = (index) => {
+    setActiveTab(index);
+
+    if (index === 0) {
+      setShowScheduleTab(true);
+    } else {
+      setShowScheduleTab(false);
+    }
+  };
   return (
-    <View style={styles.container}>
-      <Text>HousekeeperProfile Screen</Text>
-    </View>
+    <SafeAreaProvider>
+      <SafeAreaView>
+        <View style={styles.container}>
+          <View style={styles.headerContainer}>
+            <LinearGradient
+              colors={["#F89C7B", "#FFD9A5", "#FEDEB3", "#F9F9F9"]}
+              start={{ x: 0.0, y: 0.0 }}
+              end={{ x: 1.0, y: 1.0 }}
+              locations={[0.01, 0.7, 0.92, 1.0]}
+              style={styles.headerGradient}
+            >
+              <View style={styles.scheduleContainer}>
+                <CalendarIcon></CalendarIcon>
+                <Typography variant="body-medium">Feb 20 - Feb 27</Typography>
+              </View>
+              <View style={styles.profilePicAndNameContainer}>
+                <Image
+                  source={{
+                    uri: "https://picsum.photos/2000/600?random=11",
+                  }}
+                  style={styles.profilePic}
+                />
+                <View style={styles.nameContainer}>
+                  <Typography variant="title-black">Molly Chen</Typography>
+                  <Typography variant="body-medium">
+                    Housekeeper, Senior
+                  </Typography>
+                </View>
+              </View>
+            </LinearGradient>
+          </View>
+          <View style={styles.mainContainer}>
+            <NavTabs
+              screen="HousekeeperProfile"
+              tabs={tabs}
+              activeTab={activeTab}
+              onTabPress={handleTabPress}
+            />
+
+            {showScheduleTab ? (
+              <ScheduleList data={scheduleList} />
+            ) : (
+              <View style={styles.perfomanceContainer}>
+                <View style={styles.perfomanceTopButtonsContainer}>
+                  <BigButton
+                    name="Total Rooms"
+                    icon={<BedIcon w="40" h="28" fill={colors.orange} />}
+                    text="30"
+                  />
+                  <BigButton
+                    name="Average Time"
+                    icon={<ClockIcon w="40" h="28" fill={colors.orange} />}
+                    text="28 mins 16 sec"
+                    variant="xs-regular"
+                  />
+                </View>
+                <View style={styles.perfomanceBottomButtonsContainer}>
+                  <BigButton
+                    name="Rating & Feedbacks"
+                    icon={<StarIcon w="40" h="28" fill={colors.teal} />}
+                    text="12"
+                    width={300}
+                  />
+                </View>
+              </View>
+            )}
+          </View>
+        </View>
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#8fcbbc",
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: "#ffffff",
+    flexDirection: "column",
   },
+  headerContainer: { height: 140 },
+  headerGradient: {
+    width: "100%",
+    height: "100%",
+    paddingVertical: 20,
+    paddingHorizontal: 32,
+    rowGap: 20,
+  },
+  scheduleContainer: { flexDirection: "row", columnGap: 10 },
+  profilePicAndNameContainer: { flexDirection: "row" },
+  profilePic: {
+    width: 80,
+    height: 80,
+    borderRadius: 40,
+    marginRight: 10,
+    top: 20,
+    borderWidth: 6,
+    borderColor: "#FAAB85",
+  },
+  nameContainer: { flexDirection: "column", rowGap: 10, top: 6 },
+  mainContainer: {
+    flexDirection: "column",
+    paddingVertical: 20,
+    paddingHorizontal: 32,
+    // borderWidth: 1,
+    marginTop: 60,
+    height: 400,
+  },
+  perfomanceContainer: { rowGap: 60 },
+  perfomanceTopButtonsContainer: {
+    flexDirection: "row",
+    columnGap: 10,
+  },
+  perfomanceBottomButtonsContainer: {},
 });
 
 export default HousekeeperProfile;
--- src/screens/Housekeeper/HousekeeperRequest.jsx
+++ src/screens/Housekeeper/HousekeeperRequest.jsx
@@ -1,21 +1,137 @@
-import React, { useState } from "react";
-import { Button, StyleSheet, Text, View } from "react-native";
+import React, { useEffect, useState } from "react";
+import { Button, FlatList, StyleSheet, Text, View } from "react-native";
+import Typography from "../../components/Typography/Typography";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+import BigButton from "../../components/BigButton/BigButton";
+import BedIcon from "../../SVG/BedIcon";
+import { colors } from "../../../themes/themes";
+import NavTabs from "../../components/NavTabs/NavTabs";
 
 const HousekeeperRequest = ({ navigation }) => {
-  return (
-    <View style={styles.container}>
-      <Text>HousekeeperRequest Screen</Text>
+  const [activeTab, setActiveTab] = useState(0);
+  const [showPendingTab, setShowPendingTab] = useState(true);
+  const tabs = [{ label: "Pending" }, { label: "History" }];
+  const [items, setItems] = useState([]);
+  const pendingItems = [
+    { id: "1", itemName: "Toilet Paper", date: "2024-03-01" },
+    { id: "2", itemName: "Small Towel", date: "2024-03-05" },
+    { id: "3", itemName: "Large Towel", date: "2024-03-10" },
+    { id: "4", itemName: "Toilet Paper", date: "2024-03-15" },
+    { id: "5", itemName: "Small Towel", date: "2024-03-20" },
+    { id: "6", itemName: "Large Towel", date: "2024-03-20" },
+  ];
+
+  const historyItems = [
+    { id: "1", itemName: "Mopping Set", date: "2024-03-01" },
+    { id: "2", itemName: "Squeegee", date: "2024-03-05" },
+    { id: "3", itemName: "Sponge", date: "2024-03-10" },
+    { id: "4", itemName: "Mopping Set", date: "2024-03-15" },
+    { id: "5", itemName: "Squeegee", date: "2024-03-20" },
+    { id: "6", itemName: "Sponge", date: "2024-03-20" },
+  ];
+  const onRoomSuppliesPressed = () => {
+    console.log("onRoomSuppliesPressed");
+  };
+
+  const onCartSuppliesPressed = () => {
+    console.log("onCartSuppliesPressed");
+  };
+
+  const handleTabPress = (index) => {
+    setActiveTab(index);
+
+    if (index === 0) {
+      setItems(pendingItems);
+    } else {
+      setItems(historyItems);
+    }
+  };
+
+  const Item = ({ item }) => (
+    <View
+      style={{
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center",
+        borderBottomWidth: 1,
+        borderBottomColor: colors.n40,
+        paddingVertical: 16,
+        marginHorizontal: 36,
+      }}
+    >
+      <Typography variant="small-medium">{item.itemName}</Typography>
+      <Typography variant="small-medium">{item.date}</Typography>
     </View>
+  );
+
+  useEffect(() => {
+    setItems(pendingItems);
+  }, []);
+
+  return (
+    <SafeAreaProvider>
+      <SafeAreaView style={styles.container}>
+        <View style={styles.topContainer}>
+          <Typography variant="h5-black">Request Items</Typography>
+          <Typography variant="body-regular">
+            All you need in one Place!
+          </Typography>
+          <View style={styles.topButtonsContainer}>
+            <BigButton
+              name="Room Supplies"
+              icon={<BedIcon w="40" h="28" fill={colors.orange} />}
+              onPress={onRoomSuppliesPressed}
+            />
+
+            <BigButton
+              name="Cart Supplies"
+              icon={<BedIcon w="40" h="28" fill={colors.orange} />}
+              onPress={onCartSuppliesPressed}
+            />
+          </View>
+        </View>
+        <View style={styles.bottomContainer}>
+          <View style={styles.navTabContainer}>
+            <NavTabs
+              screen="HousekeeperRequest"
+              tabs={tabs}
+              activeTab={activeTab}
+              onTabPress={handleTabPress}
+            />
+            <FlatList
+              data={items}
+              renderItem={({ item }) => <Item item={item} />}
+              keyExtractor={(item) => item.id}
+            />
+          </View>
+        </View>
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#8fcbbc",
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: "#ffffff",
+    flexDirection: "column",
+    paddingHorizontal: 20,
+    paddingVertical: 10,
   },
+  topContainer: {
+    rowGap: 6,
+  },
+  topButtonsContainer: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+  },
+  bottomContainer: {},
+  navTabContainer: {
+    // borderColor: "black",
+    // borderWidth: 1,
+  },
+  pendingContainer: {},
+  historyContainer: {},
 });
 
 export default HousekeeperRequest;
