ac482b1f0f72f8ca1e3da8a6ed2f87d203cd4a1b
Cylvia <cylvia.lian@gmail.com>
2024-03-15 21:13:44 -0700

    assign room in management dashboard, loading page

--- src/hooks/useBaseUrl.js
+++ src/hooks/useBaseUrl.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 
 const useBaseUrl = () => {
-  const [baseUrl, setBaseUrl] = useState("http://10.0.2.2:5000");
+  const [baseUrl, setBaseUrl] = useState("http://192.168.1.70:5000");
 
   // useEffect(() => {
   //   const url = "http://10.2.126.67:5000"; // Default for development
